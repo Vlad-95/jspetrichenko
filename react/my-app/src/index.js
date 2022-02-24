a9fb8be3cb5554c84a60505c68e715e170b43815
Vladislav <vzv@siluetstudio.com>
2022-02-24 17:00:28 +0300

    reacr app

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//const elem = <h2>Hello World</h2>; // JSX препроцессор
+//const elem = React.createElement('h2', {className: 'someclass'}, 'Hello World'); //Еще один вариант создания элемента
+
+const text = 'Hello';
+
+const elem = (
+  <div>
+    <h2 className='ss'>Текст: {text}:{2+2}</h2>
+    <input type="text" name="" id="" />
+    <button tabIndex={0}>Click</button>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  elem,
+  document.getElementById('root')
+);
