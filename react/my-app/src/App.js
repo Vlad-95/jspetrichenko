9b0e7392fe6b524a8b0046e4bda41865e8e6a733
vlad_z <vzv@siluetstudio.com>
2022-03-03 19:37:37 +0300

    add react app

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello world</h2>
+};
+
+// const Field = () => {
+//   const styledField = {
+//     width: '300px'
+//   };
+
+//   return <input type="text" placeholder='Type here' style={styledField} type='Text'/>
+// }
+
+class Field extends Component {
+  render() { // Главный метод, он должен быть обязательно
+    const styledField = {
+      width: '300px'
+    };
+  
+    return <input type="text" placeholder='Type here' style={styledField} type='Text'/>
+  }
+}
+
+function Btn() {
+  const text = 'Log in';
+  // const res = () => {
+  //   return 'Log in + s'
+  // }
+  // const p = <p>Haha</p>
+
+  const logged = true;
+
+  // return <button>{res()}</button>
+  //return <button>{p}</button>
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      {/* <Header/> */}
+      <Field/>
+      <Btn/>
     </div>
   );
 }
-
+export {Header}
 export default App;
--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-//const elem = <h2>Hello World</h2>; // JSX препроцессор
-//const elem = React.createElement('h2', {className: 'someclass'}, 'Hello World'); //Еще один вариант создания элемента
-
-const text = 'Hello';
-
-const elem = (
-  <div>
-    <h2 className='ss'>Текст: {text}:{2+2}</h2>
-    <input type="text" name="" id="" />
-    <button tabIndex={0}>Click</button>
-  </div>
-);
+import {Header} from './App';
 
 ReactDOM.render(
-  elem,
+  <StrictMode>
+    <Header></Header>
+    <App/>
+  </StrictMode>,
   document.getElementById('root')
 );
+
