6053d71ec4ed875c7e35c14adebd3892279cd8c0
vlad_z <vzv@siluetstudio.com>
2022-03-10 19:47:34 +0300

    add script for forms

--- react/employers/src/components/employers-add-form/employers-add-form.js
+++ react/employers/src/components/employers-add-form/employers-add-form.js
@@ -1,23 +1,47 @@
+import { Component } from 'react';
+
 import './employers-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        const {name, salary} = this.state;
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input onChange={this.onValueChange} type="text"
+                        className="form-control new-post-label"
+                        name="name"
+                        value={name}// этот атрибут делает инпут управляемым. Введенное значение сразу передается в value
+                        placeholder="Как его зовут?" />
+                    <input onChange={this.onValueChange} type="number"
+                        className="form-control new-post-label"
+                        name="salary"
+                        value={salary}// этот атрибут делает инпут управляемым. Введенное значение сразу передается в value
+                        placeholder="З/П в $?" />
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployeesAddForm;
