63a7ab1402a94170dbe6d118ea5ae3e97ed0c378
vlad_z <vzv@siluetstudio.com>
2022-03-23 19:48:50 +0300

    side info

--- react/marvel/src/services/MarvelService.js
+++ react/marvel/src/services/MarvelService.js
@@ -28,11 +28,13 @@ class MarvelService {
 
     _transformCharacter = (character) => {
         return {
+            id: character.id,
             name: character.name,
             description: (character.description) ? `${character.description.slice(0, 210)}...` : 'Empty description',
             thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
             homepage: character.urls[0].url,
-            wiki: character.urls[1].url   
+            wiki: character.urls[1].url,
+            comics: character.comics.items   
         }
     }
 }
