d3863daa1709c1c72ecfadc8bd483393f3186da4
vlad_z <vzv@siluetstudio.com>
2022-03-27 11:20:41 +0300

    refs

--- react/marvel/src/components/charList/CharList.js
+++ react/marvel/src/components/charList/CharList.js
@@ -6,7 +6,6 @@ import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 class CharList extends Component {
-
     state = {
         charList: [],
         loading: true,
@@ -32,14 +31,14 @@ class CharList extends Component {
         // })
     }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll')
-    }
+    // componentWillUnmount() {
+    //     window.removeEventListener('scroll')
+    // }
     onRequest = (offset) => {
         this.onCharListLoading();
         this.marvelService.getAllCharacters(offset)
-        .then(this.onCharListLoaded)
-        .catch(this.onError)
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
     }
 
     onCharListLoading = () => {
@@ -71,19 +70,49 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        // Я реализовал вариант чуть сложнее, и с классом и с фокусом
+        // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+        // На самом деле, решение с css-классом можно сделать, вынеся персонажа
+        // в отдельный компонент. Но кода будет больше, появится новое состояние
+        // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+        // По возможности, не злоупотребляйте рефами, только в крайних случаях
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     // Этот метод создан для оптимизации, 
     // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = (item.thumbnail.includes('image_not_available')) ? {'objectFit' : 'unset'} : {'objectFit' : 'cover'};
             
             return (
                 <li 
                     className="char__item"
+                    tabIndex={0}
                     key={item.id}
-                    onClick={() => {this.props.onCharSelected(item.id)}}>
-                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                        <div className="char__name">{item.name}</div>
+                    ref={this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
+                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                    <div className="char__name">{item.name}</div>
                 </li>
             )
         });
