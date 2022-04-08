db75d78c8db8634d4e7e0957bfa9b03096f04173
Vladislav <vzv@siluetstudio.com>
2022-04-08 10:54:12 +0300

    get all comics

--- react/marvel/src/components/comicsList/ComicsList.js
+++ react/marvel/src/components/comicsList/ComicsList.js
@@ -50,7 +50,7 @@ const ComicsList = (props) => {
                     <a href="#">
                         <img src={item.thumbnail} style={imgStyle} alt={item.title} className="comics__item-img"/>
                         <div className="comics__item-name">{item.title}</div>
-                        <div className="comics__item-price">{item.price}$</div>
+                        <div className="comics__item-price">{item.price}</div>
                     </a>
                 </li>
             )
--- react/marvel/src/services/MarvelService.js
+++ react/marvel/src/services/MarvelService.js
@@ -48,8 +48,11 @@ const useMarvelService = () => {
         return {
             id: comics.id,
             title: comics.title,
-            price: comics.prices[0].price,
+            description: comics.description || 'There is no description',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
             thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            language: comics.textObjects.language || 'en-us',
+            price: comics.prices[0].price ? `${comics.prices[0].price}$` : 'not available'
         }
     }
 
