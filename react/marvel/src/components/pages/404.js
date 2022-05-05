16cc4b5154664ddb81316a692a41a2f9eaf736af
vlad_z <vzv@siluetstudio.com>
2022-05-05 19:48:43 +0300

    add helmet

--- react/marvel/src/components/pages/404.js
+++ react/marvel/src/components/pages/404.js
@@ -1,9 +1,18 @@
+import { Helmet } from 'react-helmet';
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Страница ошибки"
+                />
+                <title>404</title>
+            </Helmet>
+
             <ErrorMessage/>
             <p style={{"display": "block", "textAlign": "center"}}>Ошибка</p>
             <Link style={{"display": "block", "textAlign": "center"}} to="/">Back to main page</Link>
