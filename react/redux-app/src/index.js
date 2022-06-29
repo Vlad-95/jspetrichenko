bca4681d4bb39c70bcc8e260fdeb101d25bfb222
vlad_z <vzv@siluetstudio.com>
2022-06-29 20:55:58 +0300

    correct

---
+++ react/redux-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
