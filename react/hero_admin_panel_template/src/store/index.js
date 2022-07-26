de58c3f1eadb171a6cac19a8667599239c1f7339
Vladislav <vzv@siluetstudio.com>
2022-07-26 16:41:12 +0300

    rtk query

--- react/hero_admin_panel_template/src/store/index.js
+++ react/hero_admin_panel_template/src/store/index.js
@@ -1,6 +1,6 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import heroes from '../components/heroesList/heroesSlice';
+import { configureStore } from '@reduxjs/toolkit';
 import filters from '../components/heroesFilters/filtersSlice';
+import { apiSlice } from '../api/apiSlice';
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -42,8 +42,12 @@ const enhancer = (createStore) => (...args) => {
 //                     ) 
 
 const store = configureStore({
-    reducer: {heroes, filters},
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    reducer: { 
+        filters, 
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware()
+                                            .concat(stringMiddleware, apiSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production',
 })
     
